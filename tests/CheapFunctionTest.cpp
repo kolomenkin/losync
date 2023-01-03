59a6d20d6c19d46a5406158f473533ac8fe8505c
Sergey Kolomenkin <kolomenkin@gmail.com>
2023-01-03 18:08:01 +0400

    tests: add more tests for cheap_function

--- tests/CheapFunctionTest.cpp
+++ tests/CheapFunctionTest.cpp
@@ -1,7 +1,19 @@
-#include "losync/cheap_function.h"
+#include <losync/cheap_function.h>
 
 #include <gtest/gtest.h>
 
+#include <memory>
+
+
+TEST(CheapFunction, Construct)
+{
+    int callCount = 0;
+    auto functor = [&callCount]() { ++callCount; };
+    {
+        cheap_function<void()> func(std::move(functor));
+    }
+    EXPECT_EQ(callCount, 0);
+}
 
 TEST(CheapFunction, Basic)
 {
@@ -15,4 +27,17 @@ TEST(CheapFunction, Basic)
         EXPECT_EQ(callCount, 1);
     }
     EXPECT_EQ(callCount, 1);
+}
+
+TEST(CheapFunction, CanHandleMoveOnlyLambda)
+{
+    auto ptr = std::make_unique<int>(123);
+    auto functor = [ptr = std::move(ptr)]() { return *ptr; };
+    EXPECT_EQ(functor(), 123);
+
+    cheap_function<int()> func(std::move(functor));
+    EXPECT_EQ(func(), 123);
+
+    cheap_function<int()> func2(std::move(func));
+    EXPECT_EQ(func2(), 123);
 }
