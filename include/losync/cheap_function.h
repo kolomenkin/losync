7204cf6b84d17cf172d40503ab03c6ac977db146
Sergey Kolomenkin <kolomenkin@gmail.com>
2023-01-03 18:03:31 +0400

    docs: embed documentation copy into sources

--- include/losync/cheap_function.h
+++ include/losync/cheap_function.h
@@ -1,3 +1,11 @@
+// `cheap_function` is a template class for lightweight replacement of `std::function`.
+// Its features:
+//
+// 1. The implementation does not allocate dynamic memory.
+//    But the class is limited by the maximum size of the functor.
+// 2. The class does not support copying. It only supports move semantics.
+//    So it can wrap the lambda that captured the `std::unique_ptr`.
+
 #pragma once
 
 #include <cstddef>
